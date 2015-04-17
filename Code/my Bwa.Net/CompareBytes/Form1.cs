c6bedfdda4e02df29f01eabfe5ab3c040c94c6ad
Avi <avi.turner111@gmail.com>
2015-04-17 08:29:14 +0300

    Add bin post files

---
+++ Code/my Bwa.Net/CompareBytes/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CompareBytes
+{
+    public partial class Form1 : Form
+    {
+        FolderBrowserDialog _fDialog;
+        public Form1()
+        {
+            InitializeComponent();
+            this._fDialog = new FolderBrowserDialog();
+        }
+
+        
+
+        private void btnFolder_Click(object sender, EventArgs e)
+        {
+            var txb = (sender as Button).Name == this.btnFolder1.Name ?
+                this.txbFolder1 : this.txbFolder2;
+            if(this._fDialog.ShowDialog() == DialogResult.OK)
+            {
+                txb.Text = this._fDialog.SelectedPath;
+            }
+
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                this.label1.Text = String.Empty;
+                List<string> files1 = Directory.EnumerateFiles(this.txbFolder1.Text).ToList();
+                List<string> files2 = Directory.EnumerateFiles(this.txbFolder1.Text).ToList();
+                if (files1.Count != files2.Count)
+                {
+                    this.label1.Text = String.Format("Folder 1 had {0} files, while folder 2 has {1} files",
+                        files1.Count,
+                        files2.Count);
+                }
+                var count = files1.Count;
+                var bytes1 = new List<byte[]>(count);
+                var bytes2 = new List<byte[]>(count);
+
+
+                files1.ForEach(f => bytes1.Add(File.ReadAllBytes(f)));
+                files2.ForEach(f => bytes2.Add(File.ReadAllBytes(f)));
+
+                var noMatches = bytes1.Where(b => !bytes2.Contains(b)).ToList();//bytes1.All(b => bytes2.Contains(b)) && bytes2.All(b => bytes1.Contains(b));
+                bool match = noMatches.Count == 0;
+                this.label1.Text = match ? "Match" : String.Format("No match! Count ({0})", noMatches.Count);
+            }
+            catch (Exception ex)
+            {
+
+                this.label1.Text = ex.Message;
+
+            }
+
+
+            
+
+            
+        }
+
+       
+    }
+}
---
+++ Code/my Bwa.Net/bwa/Source.cpp
@@ -0,0 +1,162 @@
+#include <iostream>
+#include "backend.h"
+
+#define WINDOWS
+#ifdef WINDOWS
+	#include <direct.h>
+	#define GetCurrentDir _getcwd
+#else
+	#include <unistd.h>
+	#define GetCurrentDir getcwd
+#endif
+
+char* GetPathOfLocalFile(const char * fileName)
+{
+	char cwd[1024];
+	if (getcwd(cwd, sizeof(cwd)) != NULL)
+		fprintf(stdout, "Current working dir: %s\n", cwd);
+	else
+	{
+		perror("getcwd() error");
+		return NULL;
+	}
+	
+	char* out = (char* )malloc(strlen(cwd) + strlen(fileName) + 1);
+
+	if((out = (char *)malloc(strlen(cwd) + strlen(fileName) + 1)) != NULL)
+	{
+		strcpy(out, cwd);
+		strcat(out, fileName);
+	}
+	else
+	{
+		//you don't have enough memory, handle it
+	}
+
+	return out;
+}
+char* GetBinaryFileName(int idx)
+{
+	char fileName[1024];
+	sprintf(fileName,"\\seq_bin\\sequence (%d).bin",idx+1);	
+	char* out = GetPathOfLocalFile(fileName);
+
+	return out;
+}
+
+char* GetBwtFileName()
+{
+	char * fileName = "\\human_g1k_v37.fasta.bwt";
+	char* out = GetPathOfLocalFile(fileName);
+
+	return out;
+
+}
+
+
+char* readFileBytes(const char *name)  
+{  
+	FILE *fl = fopen(name, "r");  
+	fseek(fl, 0, SEEK_END);  
+	long len = ftell(fl);  
+	char *ret = (char *)malloc(len);  
+	fseek(fl, 0, SEEK_SET);  
+	fread(ret, 1, len, fl);  
+	fclose(fl);  
+	return ret;  
+} 
+
+ubyte_t*  GetSequencyBytes(char* fileName)
+{		
+	ubyte_t* bytes = (ubyte_t*)readFileBytes(fileName);
+	return bytes;
+}
+
+bwt_t *GetBwt()
+{
+	char* fileName = GetBwtFileName();
+	bwt_t * bwt = bwt_restore_bwt(fileName);
+	return bwt;
+}
+
+int file_exists(const char * filename)
+{
+	if (FILE * file = fopen(filename, "r"))
+	{
+		fclose(file);
+		return 1;
+	}
+	return 0;
+}
+
+
+bwa_seq_t* GetAllSequencies(int* seqsCount)
+{
+	char* fileName = NULL;
+	bwa_seq_t* seqs = NULL;
+	int count = 0;
+
+	while ((fileName = GetBinaryFileName(count)) != NULL)
+	{
+		if(	file_exists(fileName)) {
+			count++;
+		}else
+		{
+			break;
+		}
+		
+	}
+
+	//allocate for all sequences
+	seqs = (bwa_seq_t*)calloc(sizeof(bwa_seq_t),count);
+	bwa_seq_t* first = seqs;
+
+	//I am aware nothing guaranties that the file is still there or available, I just don't care at the moment. Quick and dirty it is....
+	int idx = 0;
+	for(idx= 0; idx < count; idx++) {
+		fileName = GetBinaryFileName(idx);
+		ubyte_t* bytes = GetSequencyBytes(fileName);
+		bwa_seq_t* currSeqs = FromBytes(bytes);
+		*seqs = *currSeqs ;
+		seqs++;		
+	}
+	*seqsCount = count;
+	return first;
+}
+
+int main(int argc, char* argv[])
+{
+	std::cout << "Hello Avis\n";
+	
+	//---------------------------
+	int seqsCount = 0;
+	bwa_seq_t* seqs = GetAllSequencies(&seqsCount);	
+	//----------------------------------
+	bwt_t *bwt = GetBwt();
+	//----------------------------------
+	
+
+	int tid = 0;	
+	int n_seqs =seqsCount;//41; 
+	gap_opt_t *opt = (gap_opt_t *)malloc(sizeof(gap_opt_t ));
+	opt->s_mm = 3;
+	opt->s_gapo = 11;
+	opt->s_gape = 4;
+	opt->mode = 3;
+	opt->indel_end_skip = 5;
+	opt->max_del_occ = 10;
+	opt->max_entries = 2000000;
+	opt->fnr = 0.39999991;
+	opt->max_diff = -1;
+	opt->max_gapo = 1;
+	opt->max_gape = 6;
+	opt->max_seed_diff = 2;
+	opt->seed_len=32;
+	opt->n_threads = 1;
+	opt->max_top2 = 30;
+	opt->trim_qual = 0;
+	bwa_cal_sa_reg_gap(tid, bwt, n_seqs, seqs, opt);
+	
+
+	return 0;
+}
