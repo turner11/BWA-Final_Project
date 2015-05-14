e43413a140b5368afe6e1cfbd00fac8b1d13f9d4
turner11 <avi.turner111@gmail.com>
2015-05-14 21:55:56 +0300

    Added Cuda solution template

---
+++ Code/bwa_new/bwa/utils.h
@@ -0,0 +1,23 @@
+// #if defined ( WIN32 )
+// #define __func__ __FUNCTION__
+// #endif
+// 
+#ifndef __func__
+#define __func__ __FUNCTION__
+#endif
+
+#define err_fatal_simple(msg) _err_fatal_simple(__func__, msg)
+#define err_fatal_simple_core(msg) _err_fatal_simple_core(__func__, msg)
+
+#define xopen(fn, mode) err_xopen_core(__func__, fn, mode)
+#define xreopen(fn, mode, fp) err_xreopen_core(__func__, fn, mode, fp)
+#define xzopen(fn, mode) err_xzopen_core(__func__, fn, mode)
+
+#define xassert(cond, msg) if ((cond) == 0) _err_fatal_simple_core(__func__, msg)
+
+int err_fseek(FILE *stream, long offset, int whence);
+size_t err_fread_noeof(void *ptr, size_t size, size_t nmemb, FILE *stream);
+FILE *err_xopen_core(const char *func, const char *fn, const char *mode);
+ long err_ftell(FILE *stream);
+ int err_fclose(FILE *stream) ;
+
